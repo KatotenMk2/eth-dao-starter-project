80ffc032be9ae0dba78082747ea98b91ac6149e3
KatotenMk2 <cino1031@gmail.com>
2022-10-17 21:53:18 +0900

    add banner/license/readme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import Head from '../components/head';
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mainnet;
@@ -8,6 +9,7 @@ const activeChainId = ChainId.Mainnet;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head/>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
